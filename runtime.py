ed1d738789f6aee20104ed399e3ac7dad8726e82
Lakshmi Chethana <163857876+klu2300030960@users.noreply.github.com>
2026-07-06 19:59:29 +0530

    Add files via upload

---
+++ runtime.py
@@ -0,0 +1,121 @@
+'''a=2
+b=4
+print(a+b)'''
+
+'''a=6
+b=8
+c=a+b
+print(c)'''
+
+#print(c)
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''a=float(input())
+b=float(input())
+print(a+b)'''
+
+'''a=str(input())
+b=str(input())
+print(a+b)'''
+
+'''a=input()
+b=input()
+print(a+b)'''
+
+'''fn=input()
+ln=input()
+print((fn+" "+ln).title())'''
+
+'''a=complex(input())
+b=complex(input())
+print(a+b)'''
+
+'''a=bool(input())
+b=bool(input())
+c=bool(input())
+print(a+b+c)'''
+
+#option
+'''a=int(input())
+b=int(input())
+option=int(input("1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input())
+b=int(input())
+option=int(input(1.add
+                 2.sub
+                 3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input())
+b=int(input())
+option=int(input("add sub mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''a=input()
+print(a)'''
+
+#swapping(1)
+'''a=input()
+b=input()
+temp=a
+a=b
+b=temp
+print(a+" "+b)'''
+
+#using tuple(2)
+'''a=input()
+b=input()
+a,b = b,a
+print("a =", a)
+print("b =", b)'''
+
+#3
+'''a=input()
+b=input()
+print(b)
+print(a)'''
+
+#4
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping\na=%d\nb=%d" %(a,b))'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping\na=%.2f\nb=%.2f" %(a,b))'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping\na=%s\nb=%s" %(a,b))'''
+
+a=input()
+b=input()
+temp=a
+a=b
+b=temp
+print("after swapping\na=%s\nb=%s" %(a,b))
+
