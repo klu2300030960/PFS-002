9c994b2ea2ac1845ebc5027d12a82cf0fa727102
Lakshmi Chethana <163857876+klu2300030960@users.noreply.github.com>
2026-07-07 20:02:43 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,121 @@
+#coditions
+'''a=2
+b=4
+if a<b:
+    print("True")'''
+
+'''a=10
+b=20
+if a>b:
+    print("True")'''
+
+'''a=12
+b=14
+if a<=b:
+    print("True")'''
+
+'''a=7
+b=9
+if a!=b:
+    print("not equal")'''
+
+'''a=9
+b=9
+if a==b:
+    print("Equal")'''
+
+'''a=input()
+if a=="java":
+    print("Equal")'''
+
+'''a=19
+b=10
+if a>=b:
+    print("True")'''
+
+'''a="chethana"
+if a=="pooja":
+    print("Yes")
+else:
+    print("No")'''
+
+#logicalmoperators
+'''a=4
+b=9
+if a<b and b>a:
+    print("Less")'''
+
+'''a=4
+b=9
+if a<=b and b>=a:
+    print("Less")'''
+
+'''a=0
+b=9
+if a!=b and b!=a:
+    print("True")'''
+
+'''a=4
+b=9
+if a!=b or a==b:
+    print("Less")'''
+
+'''a=13
+b=15
+if not a<b:
+    print("Less")'''
+
+'''a=13
+b=15
+if not a >b:
+    print("Less")'''
+
+'''a=13
+b=15
+if not a<b and b>a:
+    print("Less")'''
+
+'''a = int(input())
+b = int(input())
+if a<b and b>a:
+    print("Less")'''
+
+#identifier
+
+'''a=5
+if type(a) is int:
+    print("It is int")'''
+
+'''a=6.0
+if type(a) is not int:
+    print("False")'''
+
+'''a=int(input())
+if type(a) is int:
+    print("it is int")'''
+
+'''a="ch"
+if type(a) is not int:
+    print("False")'''
+
+#using membership
+'''a=2,3,4,5,6,7,8,9,10
+if 10 in a:
+    print("true")'''
+
+'''a=2,3,4,5,6,7,8,9,10
+if 20 in a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=int(input())
+if 30 in a:
+    print("false")'''#error
+
+'''a=2,3,4,5,6,7,8,9,10
+b=int(input())
+if b in a:
+    print("true")'''
+
+
