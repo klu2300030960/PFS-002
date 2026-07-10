3607fa0bdf5f5d2fdd845269c31de7426bd76239
Lakshmi Chethana <163857876+klu2300030960@users.noreply.github.com>
2026-07-10 19:50:56 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,112 @@
+#loops
+#for loop
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=",")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=(4,5,6,7,8,9)
+for i in a:
+    print(i)'''
+
+'''a=(4,5,6,7,8,9)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={4,5,6,7,8,9}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''d={"year":2006,"month":"june","date":21}
+for i in d:
+    print(i)
+for i in d.keys():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.values():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.items():
+    print(i)
+    print(type(d))
+    print(type(i))'''
+
+#in str
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+'''a=[3.4,5.6]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=["python","java","html","css"]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+#in bool
+
+'''a=[True,False]
+for i in a:
+    print(i)'''
+
+'''a=[True,False]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+#taks
+#without list
+#a = ["apple", "banana", "mango"]
+'''for i in fruits:
+    print(i.upper(),end=",")'''
+
+#with list
+'''b=str(a)
+print(b.upper())'''
+
+'''b =[]
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+#using extend
+'''a=[10,20,30,40,50,"code"]
+a.extend("code")
+print(a)'''
+
+'''a=[2,3,5,6,7]
+a.insert(2,4)
+print(a)'''
+
---
+++ tasks.py
@@ -0,0 +1,12 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> b=(5,6,7,8,9,10)
+>>> c=list(b)
+>>> c
+[5, 6, 7, 8, 9, 10]
+>>> c.remove(10)
+>>> c
+[5, 6, 7, 8, 9]
+>>> d=tuple(c)
+>>> d
+(5, 6, 7, 8, 9)
